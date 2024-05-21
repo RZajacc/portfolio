f287e59e08a72d17b25392b8737d56db66bac970
RZajacc <rf.zajac@gmail.com>
2024-05-21 13:05:31 +0200

    added a named grid for experience section, that changes view from 640px onwards

--- src/app/_components/Experience.tsx
+++ src/app/_components/Experience.tsx
@@ -7,9 +7,11 @@ function Experience({}: Props) {
   return (
     <section id="experience">
       <h4 className="font-bold">EXPERIENCE</h4>
-      <div className="mt-3">
-        <h6 className="font-bold text-amber-500">Full Stack Web Development</h6>
-        <p className="font-bold ">
+      <div className="exp-entry mt-3">
+        <h6 className="exp-entry__title font-bold text-amber-500">
+          Full Stack Web Development
+        </h6>
+        <p className="exp-entry__location font-bold">
           <a
             href="https://www.codeacademyberlin.com/"
             target="_blank"
@@ -18,15 +20,17 @@ function Experience({}: Props) {
             Code Academy Berlin
           </a>
         </p>
-        <small className="text-gray-400">07/2023 - 12-2023</small>
-        <p>
+        <small className="exp-entry__date text-gray-400">
+          07/2023 - 12-2023
+        </small>
+        <p className="exp-entry__description">
           Full-Time on site coding bootcamp covering 840h of practical project
           based work in SCRUM methodology.
         </p>
       </div>
-      <div className="mt-3">
-        <h6 className="font-bold text-amber-500">Scout</h6>
-        <p className="font-bold">
+      <div className="exp-entry mt-3">
+        <h6 className="exp-entry__title font-bold text-amber-500">Scout</h6>
+        <p className="exp-entry__location font-bold">
           <a
             href="https://www.berlin-recycling-volleys.de/"
             target="_blank"
@@ -35,8 +39,8 @@ function Experience({}: Props) {
             Berlin Recycling Volleys
           </a>
         </p>
-        <small className="text-gray-400">2019 - 2023</small>
-        <p>
+        <small className="exp-entry__date text-gray-400">2019 - 2023</small>
+        <p className="exp-entry__description">
           A game analyst who uses self-built Python tools in his daily work to
           present data to the team in a better and more readable way.
         </p>

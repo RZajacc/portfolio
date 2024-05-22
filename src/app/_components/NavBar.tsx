152aafbf11b4ba4cf4ce2e28567056f6b5da4d5a
RZajacc <rf.zajac@gmail.com>
2024-05-22 11:49:27 +0200

    removed for now my attempts to use translations to merge cursor improvements

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { MouseEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 

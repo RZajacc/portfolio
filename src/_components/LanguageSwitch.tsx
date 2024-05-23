f6dfd8f7fc4eaf03d2fd5cb28056eaf1e0802e63
RZajacc <rf.zajac@gmail.com>
2024-05-23 13:10:20 +0200

    finished language functionality

--- src/_components/LanguageSwitch.tsx
+++ src/_components/LanguageSwitch.tsx
@@ -1,7 +1,8 @@
 "use client";
-
 import { Locale } from "@/app/[lang]/dictionaries";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 type Props = {
   lang: {
@@ -18,17 +19,35 @@ const SwitchLang = ({ lang }: Props) => {
     router.push(`/${lang}`);
   };
 
+  // Handling selecting language
+  const path = usePathname();
+  const loc = path.split("/")[1] as Locale;
+  const [currentLang, setCurrentLang] = useState<Locale>(loc);
+  const handleLangSel = (e: ChangeEvent<HTMLSelectElement>) => {
+    setCurrentLang(e.target.value as Locale);
+    switchLang(e.target.value as Locale);
+  };
   return (
-    <div id="lang" className="space-x-3 text-center">
-      <button className="cursor-none" onClick={() => switchLang("en")}>
-        {lang.en}
-      </button>
-      <button className="cursor-none" onClick={() => switchLang("de")}>
-        {lang.de}
-      </button>
-      <button className="cursor-none" onClick={() => switchLang("pl")}>
-        {lang.pl}
-      </button>
+    <div id="lang" className="space-x-1 text-center">
+      {/* TEST */}
+      {currentLang === "pl" && (
+        <span className="fi fi-pl fis rounded-full"></span>
+      )}
+      {currentLang === "en" && (
+        <span className="fi fi-gb fis rounded-full"></span>
+      )}
+      {currentLang === "de" && (
+        <span className="fi fi-de fis rounded-full"></span>
+      )}
+      <select
+        value={currentLang}
+        onChange={handleLangSel}
+        className=" rounded-sm bg-slate-500 py-1"
+      >
+        <option value="en">&#x1F1EC;&#x1F1E7;</option>
+        <option value="de">&#x1F1E9;&#x1F1EA;</option>
+        <option value="pl">&#x1F1F5;&#x1F1F1;</option>
+      </select>
     </div>
   );
 };

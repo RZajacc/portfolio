6804c4538dc9100107f28b894db9013366251429
RZajacc <rf.zajac@gmail.com>
2024-05-24 12:47:04 +0200

    removed previous cursor settings together with styling to stick with the cursor I made

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -10,75 +10,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Connect cursor location with cursor image made in the div
-  const cursorStyle = (e: MouseEvent<HTMLBodyElement>) => {
-    let cursor = document.querySelector("#cursor") as HTMLDivElement;
-    cursor.style.top = e.pageY - scrollY + "px";
-    cursor.style.left = e.pageX - scrollX + "px";
-  };
-
-  useEffect(() => {
-    // Select the cursor element
-    let cursor = document.querySelector("#cursor") as HTMLDivElement;
-
-    // Remove cursor if touchscreen is being used
-    const handleTouch = () => {
-      // If mouse pointer was moved previously I will delete pointer here
-      const isHidden = cursor.classList.contains("hidden");
-      if (!isHidden) {
-        cursor.classList.add("hidden");
-      }
-    };
-
-    // Add the cursor on mouse move
-    const handleMouseMove = () => {
-      // Cursor is hidden as default
-      const isHidden = cursor.classList.contains("hidden");
-      if (isHidden) {
-        cursor.classList.remove("hidden");
-      }
-    };
-
-    // Add the cursor when action with mouse is made
-    const handleMouseClick = () => {
-      // Cursor is hidden as default
-      const isHidden = cursor.classList.contains("hidden");
-
-      if (isHidden) {
-        cursor.classList.remove("hidden");
-      }
-    };
-
-    const handleClickEvent = () => {
-      // If mouse pointer was moved previously I will delete pointer here
-      const isHidden = cursor.classList.contains("hidden");
-      if (!isHidden) {
-        cursor.classList.add("hidden");
-      }
-    };
-
-    // Fire events
-    window.addEventListener("touchstart", handleTouch);
-    window.addEventListener("touchend", handleTouch);
-    window.addEventListener("mousemove", handleMouseMove);
-    window.addEventListener("mousedown", handleMouseClick);
-    window.addEventListener("mouseup", handleMouseClick);
-    window.addEventListener("click", handleClickEvent);
-
-    // Clean event listeners
-    return () => {
-      window.removeEventListener("touchstart", handleTouch);
-      window.removeEventListener("touchend", handleTouch);
-      window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("mousedown", handleMouseClick);
-      window.removeEventListener("mouseup", handleMouseClick);
-    };
-  }, []);
-
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className} onMouseMove={cursorStyle}>
-        <div id="cursor" className="hidden"></div>
+      <body className={inter.className}>
         <main className="mt-[55px] min-h-screen bg-gradient-to-r from-blue-950 to-sky-950 p-3 text-justify text-slate-100 ">
           {children}
         </main>

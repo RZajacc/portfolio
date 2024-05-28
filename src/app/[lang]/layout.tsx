0e56d4159ccbd4bbbbf0991b73baceed61fbe792
RZajacc <rf.zajac@gmail.com>
2024-05-28 11:24:49 +0200

    changed the way fonts are used. I used normal imports before, now its with the use of in built nextjs functionality

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { MouseEvent, useEffect } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -12,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <main className="mt-[55px] min-h-screen bg-gradient-to-r from-blue-950 to-sky-950 p-3 text-justify text-slate-100 ">
           {children}
         </main>

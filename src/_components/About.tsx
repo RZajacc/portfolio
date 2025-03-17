fd6b06fc35c7df39a50b05afe174eefc4aafa030
Rafal Zajac <rf.zajac@gmail.com>
2025-03-17 11:53:15 +0100

    updated and styled a bit about section in english version

--- src/_components/About.tsx
+++ src/_components/About.tsx
@@ -3,21 +3,9 @@ import React from "react";
 type Props = {
   lang: {
     title: string;
-    p1: {
-      part1: string;
-      part2: string;
-      part3: string;
-    };
-    p2: {
-      part1: string;
-      part2: string;
-    };
-    p3: {
-      part1: string;
-      part2: string;
-      part3: string;
-      part4: string;
-    };
+    p1: string;
+    p2: string;
+    p3: string;
     p4: string;
   };
 };
@@ -26,33 +14,10 @@ function About({ lang }: Props) {
   return (
     <section id="about">
       <h4 className="font-bold">{lang.title}</h4>
-      <p>
-        {lang.p1.part1}
-        <a
-          href="https://www.codeacademyberlin.com/"
-          className="cursor-myhand font-bold hover:animate-pulse"
-          target="_blank"
-        >
-          Code Academy Berlin
-        </a>
-        {lang.p1.part2}
-        <span className="font-bold text-fuchsia-400">
-          Full Stack Web Developer
-        </span>
-        {lang.p1.part3}
-      </p>
-      <p>
-        {lang.p2.part1}
-        <span className="font-bold text-fuchsia-400">Python</span>
-        {lang.p2.part2}
-      </p>
-      <p>
-        {lang.p3.part1}
-        <span className="font-bold text-fuchsia-400">{lang.p3.part2}</span>{" "}
-        {lang.p3.part3} <span className="font-bold text-fuchsia-400">Java</span>{" "}
-        {lang.p3.part4}
-      </p>
-      <p>{lang.p4}</p>
+      <p className="my-2 indent-7">{lang.p1}</p>
+      <p className="my-2 ">{lang.p2}</p>
+      <p className="my-2">{lang.p3}</p>
+      <p className="mt-2">{lang.p4}</p>
     </section>
   );
 }
